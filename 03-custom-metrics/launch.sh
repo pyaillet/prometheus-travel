2d0a8616d50312cbf8acbb0e50866bfa9f4ac149
Pierre-Yves Aillet <pyaillet@gmail.com>
2018-03-24 00:02:15 +0100

    🚀 Demos update

--- 03-custom-metrics/launch.sh
+++ 03-custom-metrics/launch.sh
@@ -3,8 +3,7 @@
 docker-compose up --build -d
 
 if [ $(uname) == "Darwin" ]; then
-    open http://localhost:8081
-    open http://localhost:8081/metrics
+    open http://localhost/travel-front/
     sleep 3
     open "http://localhost:3000/dashboard/db/alien-dashboard?orgId=1&refresh=1s&from=now-5m&to=now"
 fi
