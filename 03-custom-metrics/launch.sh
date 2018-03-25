22172f2831da865ae2eb87f8d13f75cf20225e1b
Pierre-Yves Aillet <pyaillet@gmail.com>
2018-03-25 22:24:39 +0200

    🚗💨 📈 Fix delorean dashboard #7

--- 03-custom-metrics/launch.sh
+++ 03-custom-metrics/launch.sh
@@ -4,6 +4,6 @@ docker-compose up --build -d
 
 if [ $(uname) == "Darwin" ]; then
     open http://localhost/travel-front/
-    sleep 3
-    open "http://localhost:3000/dashboard/db/alien-dashboard?orgId=1&refresh=1s&from=now-5m&to=now"
+    sleep 10
+    open "http://localhost:3000/dashboard/db/delorean?orgId=1&refresh=1s&from=now-5m&to=now"
 fi
